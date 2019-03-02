925ebd6fdfcff6eef443138a60409f29d71ac56e
yashraikar1087 <yashraikar1087@gmail.com>
2019-03-01 21:56:17 -0700

    add Search Bar component

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import SearchBar from "./SearchBar";
+class App extends React.Component {
+  state = {
+    slectedVideo: null
+  };
+
+  render() {
+    return (
+      <div className="ui container">
+        <SearchBar />
+      </div>
+    );
+  }
+}
+export default App;
